2de7540b970f59612a4a3645863b1a3d5945bdc1
m90k <m90kdeveloper@gmail.com>
2020-04-15 13:29:57 -0300

    Finalizando o Desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transaction => {
+        return transaction.type === 'income';
+      })
+      .reduce((allIncome, transaction) => {
+        const totalIncome = transaction.value + allIncome;
+        return totalIncome;
+      }, 0);
+
+    const outcome = this.transactions
+      .filter(transaction => {
+        return transaction.type === 'outcome';
+      })
+      .reduce((allOutcome, transaction) => {
+        const totalOutcome = transaction.value + allOutcome;
+        return totalOutcome;
+      }, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
